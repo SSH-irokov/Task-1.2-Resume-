12bca5688ef501f6d0aeaa4f6871e03087254064
Pasha <pavel2014@mail.ru>
2019-04-30 00:58:48 +0300

    Initial Commit

---
+++ Task 1.2 (Resume)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 1.2 (Resume)
+//
+//  Created by Паша on 30/04/2019.
+//  Copyright © 2019 Паша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
